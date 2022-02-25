00eb6580336ae9480380dae3db7708f51a45d6ad
Tyler Simoni <tyler.simoni.8@gmail.com>
2022-02-25 07:25:56 -0500

    Header changes from redesign branch

--- src/components/theme/Header/NavbarLinks/NavbarLinks.jsx
+++ src/components/theme/Header/NavbarLinks/NavbarLinks.jsx
@@ -15,7 +15,6 @@ const NavbarLinks = ({ desktop }) => {
 
   return (
     <Wrapper theme={theme} desktop={desktop}>
-      <ToggleTheme desktop={desktop} navMotionProps={navMotionProps} />
       <StyledLink to='/' theme={theme}>
         <motion.div
           whileHover={navMotionProps.whileHover}
@@ -45,6 +44,7 @@ const NavbarLinks = ({ desktop }) => {
         </motion.div>
       </StyledLink>
       <SocialIcons desktop={desktop} navMotionProps={navMotionProps} />
+      <ToggleTheme desktop={desktop} navMotionProps={navMotionProps} />
     </Wrapper>
   );
 };
